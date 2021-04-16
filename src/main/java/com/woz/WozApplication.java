9507711fa49648019c2fa888d5e6e84cb4c21b93
flaviojrSalviano <flaviojr.salviano@hotmail.com>
2021-04-16 19:53:46 -0300

    Iniciando repositorio

---
+++ src/main/java/com/woz/WozApplication.java
@@ -0,0 +1,13 @@
+package com.woz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WozApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woz/WozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
